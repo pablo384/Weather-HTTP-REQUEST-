470c276274fb8ad6287a1c0d4e3308a1d174fafb
pablo384 <pabloreinoso0@gmail.com>
2017-04-20 17:31:56 -0400

    All added

---
+++ app/src/main/java/com/proeduka/weatherpr/City.java
@@ -0,0 +1,32 @@
+package com.proeduka.weatherpr;
+
+
+
+public class City {
+
+    public int id;
+    public String name;
+
+    public City(){}
+
+    public City(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
